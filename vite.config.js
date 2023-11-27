008334bbbf01c766a48267b58f12b72fa1ac0a5f
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-11-27 02:27:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   base: '/vue3-ace-editor/',
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    outDir: 'dist', // Especifica el directorio de salida
+    rollupOptions: {
+      output: {
+        // Configuración personalizada para dividir chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Divide los módulos de node_modules en chunks separados
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Aumenta el límite de tamaño de chunk para advertencias (en KB)
+  }
+});
