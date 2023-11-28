937fc3c835f7d4252050ef45becdc5c3e7ebbb4d
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-11-28 10:57:41 -0500

    trash x2

--- src/OutlineFoldNode.ts
+++
@@ -1,8 +0,0 @@
-import type { Ace } from 'ace-builds';
-
-export interface OutlineFoldNodeModel {
-  range: Ace.Range;
-  text: string;
-  children: OutlineFoldNodeModel[];
-  parent: OutlineFoldNodeModel;
-}
